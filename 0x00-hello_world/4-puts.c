586148fe94dcad017ee116d2d036e23dd30ec25b
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-17 01:22:36 -0700

    hjdjd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 
-/**
- *
- * this method will print a message to stdout
- */
+/*  this method will print a message to stdout */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
