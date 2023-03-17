c1fbc60800a0b3b65d96a4be14e703485d7bd491
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-17 01:54:17 -0700

    hjdjd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -E $CFILE > c 
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
