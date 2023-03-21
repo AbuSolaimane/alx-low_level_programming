5bf44d5e2c41cf3a0094d345a98fab19859d6663
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-21 08:01:41 -0700

    hjdjd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	for(char i='a'; i<='z'; i++)
+		_putchar ('_');
+	_putchar ('\n');
+	return (0);
+}
