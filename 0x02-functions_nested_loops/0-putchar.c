c3107ff2a7ced01a703df32298dfc8a8ac986106
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-21 07:45:47 -0700

    hjdjd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,12 @@
 
 
 /**
- * _putchar: print a character
- * 
  * main - Entry point
- * 
+ *
  * Return: always 0
  */
 int main(void)
 {
-
 	_putchar ('_');
 	_putchar ('p');
 	_putchar ('u');
@@ -20,5 +17,5 @@ int main(void)
 	_putchar ('a');
 	_putchar ('r');
 	_putchar ('\n');
-	return(0);
+	return (0);
 }
