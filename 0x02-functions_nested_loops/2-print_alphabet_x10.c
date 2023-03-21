6b93aa565c388709af8afe61963ec31a56e6d79b
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-21 08:36:21 -0700

    hjdjd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 
 	for (i = 1; i <= 10; i++)
-		print_alphabet();
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
 }
