42be25424e1f30ea5910220ceffa305c2aee12e8
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-21 08:20:30 -0700

    hjdjd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
+/**
+ * print_alphabet - print all alphabets in lowerCase
+ */
 void print_alphabet(void)
 {
 	char i;
-	for (i='a'; i<='z'; i++)
-		_putchar ('_');
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar (i);
 	_putchar ('\n');
-	return;
 }
