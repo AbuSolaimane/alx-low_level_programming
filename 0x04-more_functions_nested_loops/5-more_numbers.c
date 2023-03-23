b1ed8e18509a6e0b212f812c13335e52fd30ef4a
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-23 14:46:29 -0700

    hjdjd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print - function
+ *
+ * @a: nuber to print
+ */
+void print (int a)
+{
+	if(a >= 10)
+		print(a / 10);
+	_putchar(a % 10);
+}
+
+/**
+ * more_numbers - function
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			print(j);
+		}
+		_putchar('\n');
+	}
+}
