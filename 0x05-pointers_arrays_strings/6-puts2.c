aaa09098ef4d7a3544e2ed52bb2219600ade0bf9
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-27 15:13:24 -0700

    hjdjd

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * void puts2 - function
+ *
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] = '\0')
+	{
+		if (i % 2 == 2)
+			putchar(str[i]);
+	}
+}
