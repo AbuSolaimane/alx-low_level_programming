d7a3837d8ee70e16b48c7cdf08ee3d20ea1561a9
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-27 15:34:53 -0700

    hjdjd

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * puts_half - function
+ *
+ * @str: parameter
+ */
+void puts_half(char *str)
+{
+	int i, len, n;
+	
+	len = strlen(str);
+	n = (strlen(str) + 1) / 2;
+
+	for (i = n; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
