ab5f19c5de47ab42b06e9aba0ff7c736fe3bc56b
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-27 14:58:02 -0700

    hjdjd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+/**
+ * rev_string - function
+ *
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	int i, lenOfS;
+	lenOfS = strlen(s);
+
+	for (i = 0; i < lenOfS / 2; i++)
+	{
+		int t = s[i];
+		s[i] = s[lenOfS - 1 - i];
+		s[lenOfS - 1 - i] = t;
+	}
+}
