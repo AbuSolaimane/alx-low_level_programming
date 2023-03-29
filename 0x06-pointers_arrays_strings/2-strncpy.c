3fc975cf7de826d182e1047f64058db636c97478
AbuSolaimane <fadilimustafa530@gmail.com>
2023-03-29 14:53:59 -0700

    hjdjd

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * _strncpy - function
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * @n: number
+ *
+ * Return: value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
