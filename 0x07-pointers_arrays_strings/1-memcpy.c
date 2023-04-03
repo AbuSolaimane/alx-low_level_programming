e9dc4d0dafb0833345e22bac587dddf6d22c6a89
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-03 14:42:55 -0700

    hjdjd

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+ * _memcpy - function
+ *
+ * @dest: parameter1
+ *
+ * @src: parameter2
+ *
+ * @n: parameter3
+ *
+ * Return: pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	_memcpy(dest, src, n);
+	return (dest);
+}
