c1907826ae8269282bc9f0f445317f4d012732c9
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-03 16:21:35 -0700

    hjdjd

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,6 @@
 
 /**
  * print_chessboard - prints the chessboard
- *
  * @a: two dimension array to print
  */
 void print_chessboard(char (*a)[8])
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strcat(char *dest, char *src);
-char *cap_string(char *)
-char *_memcpy(char *dest, char *src, unsigned int n);
+void set_string(char **s, char *to);
 
 #endif
