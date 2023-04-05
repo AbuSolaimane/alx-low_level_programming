e89a779448a3f05a44bef6bad48edb0cf9bd2488
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-05 14:35:34 -0700

    hjdjd

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - function
+ *
+ * @x: parameter1
+ *
+ * @y: parameter2
+ *
+ * Return: fact
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(y - 1));
+}
