4d122bd66701c8de6ab505e7634c7f103b9f0ff7
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-09 05:35:46 -0700

    hjdjd

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * @argc: arguments count
+ *
+ * @argv: arguments vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv + 0);
+}
