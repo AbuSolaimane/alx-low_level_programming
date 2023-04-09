baac222646be4f60d126374985615805d2dd8476
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-09 07:06:10 -0700

    hjdjd

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ *
+ * @argc: arguments count
+ *
+ * @argv: arguments vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, j;
+
+	if (argc < 3)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; *(argv[i] + j) != '\0'; j++)
+			{
+				if (isdigit(*(argv[i] + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
