77863d2242d6367e69ed80f35a6daed86538ce9e
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-13 12:58:03 -0700

    hjdjd

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - function
+ *
+ * @b: parameter1
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+
+	return ptr;
+}
