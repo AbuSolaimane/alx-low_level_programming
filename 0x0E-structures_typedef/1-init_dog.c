2fb9b22d83495e8c1fb1903ce9acec0040e7e4b5
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-17 13:12:06 -0700

    hjdjd

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		exit(EXIT_FAILURE);
 	(*d).name = name;
