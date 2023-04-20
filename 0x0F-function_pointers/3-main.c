20f1e802334a5f1b4a47a26d71df43a742f1ec38
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-19 17:01:05 -0700

    hjdjd

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
 {
 	int arg1, arg2;
 	int result;
-	int (*operation) (int, int);
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
 	arg1 = atoi(argv[1]);
@@ -23,8 +23,8 @@ int main(int argc, char *argv[])
 	operation = get_op_func(argv[2]);
 	if (operation == NULL)
 	{
-		printf ("Error\n");
-                exit(99);
+		printf("Error\n");
+		exit(99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && arg2 == 0)
 	{
