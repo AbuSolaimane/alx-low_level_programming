7fc4c81feab41f7834af6b257985243f120f118d
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-29 06:27:54 -0700

    hjdjd

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function
+ *
+ * @head: parameter
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!head)
+		return;
+	while (*head->next)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+	free(*head);
+	*head = NULL;
+}
