8fe130c2aa408f9519602eb4117e23bc5171b1c1
AbuSolaimane <fadilimustafa530@gmail.com>
2023-04-29 06:05:01 -0700

    hjdjd

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,8 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t* newNode, iterator = *head;
+	listint_t* newNode;
+	listint_t* iterator = *head;
 	
 	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
