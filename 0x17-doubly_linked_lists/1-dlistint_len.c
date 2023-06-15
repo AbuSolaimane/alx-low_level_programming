13477deac495a137e007c099a3ee5b92bc71aa12
AbuSolaimane <fadilimustafa530@gmail.com>
2023-06-15 13:49:32 -0700

    hjdjd

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function.
+ * @h: param.
+ *
+ * Return: rtn
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+		number++;
+	return (number);
+}
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Counts the number of elements in a linked dlistint_t list.
- * @h: The head of the dlistint_t list.
+ * dlistint_len - function.
+ * @h: param.
  *
- * Return: The number of elements in the dlistint_t list.
+ * Return: rtn
  */
 size_t dlistint_len(const dlistint_t *h)
 {
