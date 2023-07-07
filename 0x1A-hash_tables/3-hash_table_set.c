6372e963f18eb608651bde2e00ef84c3a76ac070
AbuSolaimane <fadilimustafa530@gmail.com>
2023-07-07 13:43:05 -0700

    hjdjd

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - func
+ *
+ * @ht: param
+ *
+ * @key: param
+ *
+ * @value: param
+ *
+ * Return: return
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newNode;
+	char *value_copy;
+	unsigned long int idx, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	newNode->key = strdup(key);
+	if (newNode->key == NULL)
+	{
+		free(newNode);
+		return (0);
+	}
+	newNode->value = value_copy;
+	newNode->next = ht->array[idx];
+	ht->array[idx] = newNode;
+
+	return (1);
+}
