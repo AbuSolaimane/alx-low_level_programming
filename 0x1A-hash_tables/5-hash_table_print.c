0181dce0d8eab670f9eeba938c19118ba9887462
AbuSolaimane <fadilimustafa530@gmail.com>
2023-07-07 13:54:23 -0700

    hjdjd

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - func
+ *
+ * @ht: param
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *my_node;
+	unsigned long int idx;
+	unsigned char flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		if (ht->array[idx] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			my_node = ht->array[i];
+			while (my_node != NULL)
+			{
+				printf("'%s': '%s'", my_node->key, my_node->value);
+				my_node = my_node->next;
+				if (my_node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
